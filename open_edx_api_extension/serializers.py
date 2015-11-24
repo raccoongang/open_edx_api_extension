55a2ba1c03e905f437e51910bad190fffdf1c91e
xahgmah <xahgmah@yandex.ru>
2015-11-24 16:22:36 +0200

    Serialize method fields

--- open_edx_api_extension/serializers.py
+++ open_edx_api_extension/serializers.py
@@ -9,58 +9,26 @@ from edx_proctoring.api import get_all_exams_for_course
 from course_structure_api.v0.serializers import CourseSerializer
 
 
-class ExamSerializerField(serializers.Field):
-    """ Serializer for examSerializerField"""
 
-    is_proctored = False
+class CourseWithExamsSerializer(CourseSerializer):
+    proctored_exams = serializers.SerializerMethodField("get_proctored_exams")
+    regular_exams = serializers.SerializerMethodField("get_regular_exams")
 
-    def __init__(self, read_only=True, *args, **kwargs):
-        if 'is_proctored' in kwargs:
-            self.is_proctored = kwargs.pop('is_proctored')
-        self.read_only = True
-        return super(ExamSerializerField, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.include_expired = kwargs.pop("include_expired", False)
+        super(CourseWithExamsSerializer, self).__init__(*args, **kwargs)
 
-    def to_representation(self, instance, exams):
-        """
-        Field value -> String.
-        """
+    def get_proctored_exams(self):
+        return self._get_exams(True)
+
+    def get_regular_exams(self):
+        return self._get_exams(False)
+
+    def _get_exams(self, is_proctored):
+        exams = get_all_exams_for_course(course_id=self.course_id)
         result = []
         for exam in exams:
             if exam.get('is_proctored') == self.is_proctored:
                 result.append(exam)
         return result
 
-
-class CourseWithExamsSerializer(CourseSerializer):
-    proctored_exams = ExamSerializerField(is_proctored=True)
-    regular_exams = ExamSerializerField()
-
-    def __init__(self, *args, **kwargs):
-        self.include_expired = kwargs.pop("include_expired", False)
-        super(CourseWithExamsSerializer, self).__init__(*args, **kwargs)
-
-    def to_representation(self, instance):
-        """
-        Object instance -> Dict of primitive datatypes.
-        """
-        ret = OrderedDict()
-        fields = [field for field in self.fields.values() if
-                  not field.write_only]
-        exams = get_all_exams_for_course(course_id=instance.id)
-        for field in fields:
-            try:
-                attribute = field.get_attribute(instance)
-            except SkipField:
-                continue
-            except AttributeError:
-                if isinstance(field, ExamSerializerField):
-                    ret[field.field_name] = field.to_representation(instance,
-                                                                    exams)
-                continue
-
-            if attribute is None:
-                ret[field.field_name] = None
-            else:
-                ret[field.field_name] = field.to_representation(attribute)
-
-        return ret
