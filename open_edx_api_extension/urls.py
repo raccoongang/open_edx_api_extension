26770ba65d8122dc6fff8846c9b908689e39a0ea
Andrey Doroshenko <dorosh@MacBook-Pro-Andrey.local>
2016-01-29 14:22:50 +0200

    cut api balls

--- open_edx_api_extension/urls.py
+++ open_edx_api_extension/urls.py
@@ -6,11 +6,11 @@ from open_edx_api_extension import views
 
 urlpatterns = [
     url(r'^courses/$', views.CourseList.as_view()),
-    url(r'^courses/proctored$', views.CourseListWithExams.as_view()),
-    url(r'^courses/{}/(?P<username>\w+)/$'.format(settings.COURSE_ID_PATTERN), views.CourseUserResult.as_view()),
-    url(r'^enrollment$', views.SSOEnrollmentListView.as_view(), name='courseenrollments'),
-    url(r'^user_proctored_exams/(?P<username>\w+)/$',
-        views.ProctoredExamsListView.as_view(), name='user_proctored_exams'),
-    url(r'^libraries/$', views.LibrariesList.as_view()),
-    url(r'^paid_mass_enrollment$', views.PaidMassEnrollment.as_view()),
+    # url(r'^courses/proctored$', views.CourseListWithExams.as_view()),
+    # url(r'^courses/{}/(?P<username>\w+)/$'.format(settings.COURSE_ID_PATTERN), views.CourseUserResult.as_view()),
+    # url(r'^enrollment$', views.SSOEnrollmentListView.as_view(), name='courseenrollments'),
+    # url(r'^user_proctored_exams/(?P<username>\w+)/$',
+    #     views.ProctoredExamsListView.as_view(), name='user_proctored_exams'),
+    # url(r'^libraries/$', views.LibrariesList.as_view()),
+    # url(r'^paid_mass_enrollment$', views.PaidMassEnrollment.as_view()),
 ]
